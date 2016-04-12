ea60a9fd74e14668e33188f6f371e7618cc2ce90
hinx <b2765270@gmail.com>
2016-04-12 18:39:54 +0800

    prod mode

--- app/App.js
+++ app/App.js
@@ -1,18 +1,9 @@
 import React from 'react-native';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
-import promise from 'redux-promise';
-
-import * as reducers from './reducers';
-
-const logger = createLogger();
-const createStoreWithMiddleware = applyMiddleware(thunk, promise, logger)(createStore);
-const reducer = combineReducers(reducers);
-const store = createStoreWithMiddleware(reducer);
-
+import { configureStore } from './store';
 import AppRoutes from './routes';
+
+const store = configureStore();
 
 export default function App() {
   return (
