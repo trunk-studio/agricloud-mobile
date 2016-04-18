67c1174718b6561e19f3b98fcc8da2e96a0e0d22
hinx <b2765270@gmail.com>
2016-04-18 18:19:51 +0800

    update cover UI

--- app/components/CoverCard.js
+++ app/components/CoverCard.js
@@ -12,14 +12,12 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgb(240, 240, 240)',
     width: windowSize.width,
     position: 'absolute',
-    top: 20,
     left: 0,
     opacity: 0.9,
   },
   text: {
     fontSize: 34,
     textAlign: 'center',
-    paddingTop: 120,
     color: '#eee',
     letterSpacing: 1,
     fontWeight: '600',
@@ -31,8 +29,12 @@ const styles = StyleSheet.create({
 export default function CoverCard(props) {
   return (
     <View>
-      <Image source={{ uri: props.uri }} style={[styles.cover, { height: props.height }]} />
-      <Text style={[styles.text, { height: props.height + 20 }]}>
+      <Image source={{ uri: props.uri }}
+        style={[styles.cover, { height: props.height, top: props.top }]}
+      />
+      <Text style={[styles.text,
+        { height: props.height + 20, paddingTop: props.height / 2 + props.txtTop }]}
+      >
         {props.title}
       </Text>
     </View>
@@ -42,12 +44,16 @@ export default function CoverCard(props) {
 CoverCard.propTypes = {
   title: React.PropTypes.string,
   index: React.PropTypes.number,
+  top: React.PropTypes.number,
+  txtTop: React.PropTypes.number,
   height: React.PropTypes.number,
   uri: React.PropTypes.string,
 };
 
 CoverCard.defaultProps = {
   title: '',
+  top: 0,
+  txtTop: 0,
   height: 200,
   uri: 'https://unsplash.it/400/400/?random',
 };
