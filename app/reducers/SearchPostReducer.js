e0d3a6dd389c91b5cec2944a10ccaea8b05aab88
hinx <b2765270@gmail.com>
2016-04-25 14:51:25 +0800

    update reducer

--- app/reducers/SearchPostReducer.js
+++ app/reducers/SearchPostReducer.js
@@ -1,9 +1,15 @@
 import {
+  RECEIVED_SEARCH_NEWS,
   RECEIVED_SEARCH_POST,
 } from '../actions/SearchActions';
 
 export function search(state = {}, action) {
   switch (action.type) {
+    case RECEIVED_SEARCH_NEWS:
+      return {
+        ...state,
+        newsList: action.data,
+      };
     case RECEIVED_SEARCH_POST:
       return {
         ...state,
